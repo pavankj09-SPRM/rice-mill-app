31f67ea4826e5b0e22b4203fe137fd07c5c9e4d9
pavankj09-SPRM <pavankj09@gmail.com>
2026-04-17 17:19:38 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -3,5 +3,5 @@ db.version(1).stores({
     hulling: '++id, name, date',
     stock: '++id, name, date, type',
     expenses: '++id, name, date',
-    settings: 'name, category' // Add this table for your Varieties
+    settings: '++id, fullName, name, category' // Added fullName for JSON compatibility
 });

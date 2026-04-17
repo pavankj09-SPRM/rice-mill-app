85017b41e0b5c0772ce9e551535d2071d056504e
pavankj09-SPRM <pavankj09@gmail.com>
2026-04-17 19:46:41 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -3,5 +3,5 @@ db.version(1).stores({
     hulling: '++id, name, date',
     stock: '++id, name, date, type',
     expenses: '++id, name, date',
-    settings: '++id, fullName, name, category' // Added fullName for JSON compatibility
+    settings: '++id, fullName, category' // Use ++id to allow auto-generation
 });
